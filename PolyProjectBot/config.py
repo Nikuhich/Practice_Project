3f2eab2f8552c0fe989838b2ab61cac2b488beef
newplays <89664080+newplays@users.noreply.github.com>
2026-05-15 00:28:40 +0300

    Бот на Python для Telegram
    Добавлен бот для вариативной части задания.
    Источник: https://www.freecodecamp.org/news/how-to-create-a-telegram-bot-using-python/

---
+++ PolyProjectBot/config.py
@@ -0,0 +1,5 @@
+settings = {
+    'token_tg': '8704929828:AAHawJksT4Cz0gZyJmqvHrjb102XDbKYVM0',
+    'token_ai': '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',
+    'bot': 'Polytech Project Bot'
+}
